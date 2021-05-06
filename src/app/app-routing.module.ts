a6c54ff576ae8528f1f3948e307fb4fb8f62a5f0
YulissaA2805 <yulissa.alvarez@cetys.edu.mx>
2021-05-06 03:35:42 -0700

    7mo commit (rutas)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrincipalComponent } from "./principal/principal.component";
+import { QuienesSomosComponent } from "./quienes-somos/quienes-somos.component";
+import { SugerenciasComponent } from "./sugerencias/sugerencias.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PrincipalComponent},
+  {path: 'quienessomos', component: QuienesSomosComponent},
+  {path: 'sugerencias', component: SugerenciasComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
